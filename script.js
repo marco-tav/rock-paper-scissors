5b15574abf9a62b378f66fc57947fbb9b89588e0
marco-tav <marco.tavella@outlook.com>
2023-09-26 01:01:35 -0300

    Add preliminary interface version
    It still needs work to look good but it works. Play again button doesn't
    work and the way the game finishes once a player reaches five points
    isn't ideal and should be dealt with next.

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function getPlayerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let theHumanWins = false;
-  let theComputerWins = false;
+  let pScore = 0;
+  let cScore = 0;
 
   if (playerSelection === computerSelection) {
     message = `It's a tie! It's ${playerSelection} vs ${computerSelection}`;
@@ -29,46 +29,43 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
       message = `The computer wins, ${computerSelection} beats ${playerSelection}!`;
-      theComputerWins = true;
+      cScore++;
     } else if (computerSelection === 'scissors') {
       message = `You win, ${playerSelection} beats ${computerSelection}!`;
-      theHumanWins = true;
+      pScore++;
     }
   
   } else if (playerSelection === 'paper') {
     if (computerSelection === 'scissors') {
       message = `The computer wins, ${computerSelection} beats ${playerSelection}!`;
-      theComputerWins = true;
+      cScore++;
     } else if (computerSelection === 'rock') {
       message = `You win, ${playerSelection} beats ${computerSelection}!`;
-      theHumanWins = true;
+      pScore++;
     }
   
   } else if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
       message = `The computer wins, ${computerSelection} beats ${playerSelection}!`;
-      theComputerWins = true;
+      cScore++;
     } else if (computerSelection === 'paper') {
       message = `You win, ${playerSelection} beats ${computerSelection}!`;
-      theHumanWins = true;
+      pScore++;
     }
   }
 
-  return {message, theHumanWins, theComputerWins};
+  return {message, pScore, cScore};
 }
 
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-  
-  for (let i = 0; i < 5; i++) {
-    let {message, theHumanWins, theComputerWins} = playRound(getPlayerSelection(), getComputerSelection());
 
-    if (theHumanWins === true) {
-      playerScore += 1;
-    } else if (theComputerWins === true) {
-      computerScore += 1;
-    }
+  for (let i = 0; i < 5; i++) {
+    let {message, pScore, cScore} = playRound(getPlayerSelection(), getComputerSelection());
+
+    playerScore += pScore;
+    computerScore += cScore;
 
     console.log(message);
   }
@@ -86,3 +83,42 @@ function game() {
 
 
 /* ------------- MAIN PROGRAM -------------- */
+
+const rpsButtons = document.querySelectorAll('.game-button');
+const messageBoard = document.querySelector('.messageboard');
+const playerScoreboard = document.querySelector('.player');
+const computerScoreboard = document.querySelector('.computer');
+
+let playerScore = 0;
+let computerScore = 0;
+let finalMessage;
+
+rpsButtons.forEach(button => button.addEventListener('click', (e) => {
+
+  let {message, pScore, cScore} = playRound(button.textContent, getComputerSelection());
+  
+  playerScore += pScore;
+  computerScore += cScore;
+
+  messageBoard.textContent = message;
+
+  playerScoreboard.textContent = playerScore;
+  computerScoreboard.textContent = computerScore;
+
+  if (playerScore === 5 || computerScore === 5) {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreboard.textContent = 0;
+  }
+
+  if (playerScore === 5) {
+    finalMessage = `You win this match ${playerScore} to ${computerScore}`;
+    messageBoard.textContent = finalMessage;
+  } else if (computerScore === 5) {
+    finalMessage = `You lose this match ${playerScore} to ${computerScore}`;
+    messageBoard.textContent = finalMessage;
+  }
+}))
+
+
+
